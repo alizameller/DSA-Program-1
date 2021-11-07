d4523e15b6062d6476f02088a1876b228485fcab
alizameller <alizameller@gmail.com>
2021-11-07 01:52:15 -0500

    added main

--- Program1.cpp
+++ Program1.cpp
@@ -2,61 +2,82 @@
 // Created by Aliza Meller on 11/3/21.
 //
 #include <iostream>
-using  std::string;
+using namespace std;
 
 template <class T>
-
-class simpleList{
+class SimpleList {
 private:
     class Node {
     private:
         T data;
-        Node* next;
+        Node *next;
 
     public:
-        T getData(){ //might not need this
+        //data and next setters
+        void setData(T val) {
+            data = val;
+        }
+
+        void setNext(Node *ptr) {
+            next = ptr;
+        }
+
+        //data and next getters
+        T getData() const {
             return data;
         }
-        Node(T x, Node* y) {
+
+        Node *getNext() const {
+            return next;
+        }
+
+        Node(T x, Node *y) {
             data = x;
             next = y;
-        };
+        }
     };
 
     string name;
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
     int size;
 
 protected:
-    virtual void insertStartNode(T val){ //push stack
+    virtual void insertStartNode(T val) { //push stack
         Node startNode(val, head);
         head = &startNode;
         return;
-    };
-    virtual void insertEndNode(T val){ //push queue
+    }
+
+    virtual void insertEndNode(T val) { //push queue
         Node endNode(val, NULL);
-        tail -> next = &endNode;
+        tail->next = &endNode;
         tail = &endNode;
         return;
-    };
-    virtual Node removeStartNode(){ //pop
+    }
+
+    virtual Node removeStartNode() { //pop
         Node removedNode = *head;
-        head = head -> next;
+        head = head->next;
         return removedNode;
-    };
+    }
 
 public:
     string getName() const {
         return name;
     };
-    virtual void push() = 0;
-    virtual Node pop() = 0;
+   // virtual void push() = 0;
+   // virtual Node pop() = 0;
 
-    simpleList(string listName) {
+    SimpleList(string listName) {
         name = listName;
         head = NULL;
         tail = NULL;
         size = 0;
     };
+};
+
+int main (){
+    SimpleList<int> myList('Adin');
+  return 0;
 };
